bfe1ab1453aeb3dd6f5946786fdce3830246d7e3
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-09-11 15:08:08 +0300

    Initial import

---
+++ sdf-csv-join.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import argparse
+import csv
+import os.path
+import sys
+from collections import defaultdict
+from collections import namedtuple
+
+
+__author__ = "Eldar Abusalimov"
+
+
+def outerr(*args, **kwargs):
+    kwargs.setdefault('file', sys.stderr)
+    print(*args, **kwargs)
+
+
+def parse_sdf(filename, id_prop):
+    result = defaultdict(dict)
+
+    outerr('{}: reading...'.format(filename), end='')
+
+    with open(filename, 'rb') as sdfile:
+        it = iter(sdfile)
+
+        for line in it:
+            if line[0] != '>':
+                continue
+
+            header = line.split()
+            prop = header[1][1:-1]
+            try:
+                mol = header[2][1:-1]
+            except IndexError:
+                mol = None
+
+            value = next(it).rstrip()
+
+            props = result[mol]
+            if prop in props:
+                raise ValueError('Conflicting property name: {}'.format(prop))
+            props[prop] = value
+
+            next(it)  # skip newline
+
+    outerr('checking...', end='')
+    for mol, props in result.iteritems():
+        if props.get(id_prop) != mol:
+            raise ValueError('ID mismatch: {} != {}'
+                             .format(props.get(id_prop), mol))
+
+    outerr('done: {} molecules'.format(len(result)))
+
+    return dict(result)
+
+
+def read_csv(filename, id_prop):
+    result = {}
+
+    with open(filename, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            mol = row[id_prop]
+            result[mol] = row
+
+    return result
+
+
+def join_results(haystack, needle, row_type, include_none=False):
+    result = dict()
+
+    for mol in needle:
+        try:
+            props = haystack[mol]
+        except KeyError:
+            if include_none:
+                result[mol] = None
+        else:
+            result[mol] = row_type(**dict((prop, props.get(prop, ''))
+                                          for prop in row_type._prop_names))
+
+    return result
+
+
+def write_csv(filename, table, row_type=None):
+    with open(filename, 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+
+        if row_type is not None:
+            writer.writerow(row_type._prop_names)
+        writer.writerows(table.itervalues())
+
+
+def print_table(table, row_type):
+    header = row_type._prop_names
+    rows = list(table.itervalues())
+
+    col_width = [max(len(x) for x in col) for col in zip(header, *rows)]
+
+    print('  '.join('{:>{}}'.format(r, w) for r, w in zip(header, col_width)))
+    for row in rows:
+        assert isinstance(row, row_type)
+        print('  '.join('{:>{}}'.format(r, w) for r, w in zip(row, col_width)))
+
+
+def create_row_type(prop_names, id_prop=None):
+    if isinstance(prop_names, basestring):
+        prop_names = prop_names.replace(',', ' ').split()
+    prop_names = map(str, prop_names)
+
+    if id_prop is not None:
+        prop_names = [id_prop] + [n for n in prop_names if n != id_prop]
+
+    row_type = namedtuple('Row', prop_names, rename=True)
+    row_type._prop_names = row_type._make(prop_names)
+
+    return row_type
+
+
+def read_input_file(filename, id_prop):
+    ext = os.path.splitext(filename)[1].lower()
+    if ext == 'csv':
+        return read_csv(filename, id_prop)
+    else:
+        if ext != 'sdf':
+            outerr('{}: will be treated as SDF'.format(filename),
+                  file=sys.stderr)
+        return parse_sdf(filename, id_prop)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output',
+                        help='CSV output file')
+    parser.add_argument('-i', '--id', type=str, default='ID',
+                        help='ID property name')
+    parser.add_argument('-p', '--props', type=str, default='',
+                        help='Properties to extract from the haystack')
+    parser.add_argument('haystack_file',
+                        help='SDF or CSV file to search in')
+    parser.add_argument('needle_files', nargs='+',
+                        help='SDF or CSV file with data to search for')
+    args = parser.parse_args()
+
+    row_type = create_row_type(args.props, id_prop=args.id)
+
+    haystack = read_input_file(args.haystack_file, id_prop=args.id)
+
+    needle = set()
+    for needle_file in args.needle_files:
+        needle |= set(read_input_file(needle_file, id_prop=args.id))
+
+    result = join_results(haystack, needle, row_type)
+
+    print_table(result, row_type)
+    if args.output is not None:
+        write_csv(args.output, result, row_type)
+
+if __name__ == '__main__':
+    main()
